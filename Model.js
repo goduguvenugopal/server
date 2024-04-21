0cb9314be102e8c31cef87d782a90681a73a5205
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-21 16:51:03 +0530

    add

--- Model.js
+++ Model.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const registerSchema = new mongoose.Schema({
     name: {
       type: String,
-      required: true,
+       
     },
     email: {
       type: String,
